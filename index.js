64bd6b307a58abccd0b567caf84f0ca236fab9b0
Rabiul Hasan <34013030+robi042@users.noreply.github.com>
2022-07-21 11:59:27 +0600

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var os = require("os");
 
 
 // App setup
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 const app = express();
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
